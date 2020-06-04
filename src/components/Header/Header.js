1210689af5fc273eb40b84a4883485e4cfe3daf4
Suzann Kowalski <suzannaudra@gmail.com>
2020-06-03 21:35:32 -0700

    updated Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Header.css";
 
-//Holds the score and changeds as the scores are clicked using props. 
+//Holds the score and changes as the scores are clicked using props. 
 const Header = props => (
   <div className="header">
-    <div className="title">Nature's Click Game</div>
+    <div className="title">Nature's Game</div>
     <div className="scores">
       Score: {props.score} Highscore: {props.highscore}
     </div>
